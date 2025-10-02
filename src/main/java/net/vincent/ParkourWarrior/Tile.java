b339ed005c0c131185b929711e3572d504ce0890
Vincent Yang <yang.vincent.h@icloud.com>
2025-10-02 21:13:54 +0800

    javadoc for Tile.java

--- src/main/java/net/vincent/ParkourWarrior/Tile.java
+++ src/main/java/net/vincent/ParkourWarrior/Tile.java
@@ -2,8 +2,30 @@ package net.vincent.ParkourWarrior;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * Tiles class for each tile.
+ * <p>
+ * This class is the parent of every tile, which are the blocks
+ * displayed inside the game, each {@code Tile} contains a
+ * {@code BufferedImage} of that tile for {@code Graphics2D} to
+ * draw. This also consist of a {@code boolean} which determines
+ * if a tile is solid.
+ * </p>
+ * @author Vincent4486
+ * @version 1.2
+ * @since 1.0
+ */
 public class Tile {
 
+   /**
+    * The {@code BufferedImage} to store the image of a tile.
+    * @since 1.0
+    */
    public BufferedImage image;
+
+   /**
+    * To see if a tile is solid.
+    * @since 1.0
+    */
    public boolean solidTile = false;
 }
